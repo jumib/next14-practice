d7abab3e3d37f85b27ad860902bff01bef38f6f8
Kaia <kaia.baek@sparkplus.co>
2024-03-21 20:38:03 +0900

    hydration

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,22 +2,24 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Navigation() {
   const path = usePathname();
-  console.log(path);
+  const [count, setCount] = useState(0);
   return (
     <nav>
       <ul>
         <li>
           <Link href={"/"}>Home</Link>
-          {path === "/" ? "@" : ""}
+          {path === "/" ? "🔥" : ""}
         </li>
         <li>
           <Link href={"/about-us"}>About Us</Link>
-          {path === "/about-us" ? "@" : ""}
+          {path === "/about-us" ? "🔥" : ""}
         </li>
       </ul>
+      <button onClick={() => setCount((c) => c + 1)}>{count}</button>
     </nav>
   );
 }
